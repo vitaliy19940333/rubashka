f455a3af80568dd94332e0dc644bc0b44913bafb
Vitaliy Borys <vitala_boris@mail.ru>
2017-01-29 01:25:33 +0200

    Change footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,36 +1,6 @@
 <div class='footer'>
 	<div class='container'>
-		<div class='row'>
-			<div class="col-md-3">
-				<h3>Информация</h3>
-				<ul class="contrast_font">
-					<li><i class="fa fa-caret-right"></i><a href="">О нас</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="">Оплата</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="">Доставка покупок</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="">Обратная связь</a></li>
-				</ul>		
-			</div>
-			<div class="col-md-3">
-				<h3>Личный Кабинет</h3>
-				<ul class="contrast_font">
-					<li><i class="fa fa-caret-right"></i><a href="">Личный Кабинет</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="">История заказов</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="d">Возврат товара</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="">Закладки</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="r">Рассылка</a></li>
-				</ul>
-			</div>
-			<div class='col-md-3'>
-				<h3>Дополнительно</h3>
-				<ul class="contrast_font">
-					<li><i class="fa fa-caret-right"></i><a href="">Производители</a></li>
-					<li><i class="fa fa-caret-right"></i><a href=""">Подарочные сертификаты</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="">Партнерская программа</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="">Акции</a></li>
-					<li><i class="fa fa-caret-right"></i><a href="">Карта сайта</a></li>
-				</ul>
-			</div>
-		</div>
+		аввпвыа
 	</div>
 </div>
 
